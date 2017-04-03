43b3c7fd29149aa468c52664a1e3a1de6f0ecbf5
Dmitry Ziganshin <linpark1234@gmail.com>
2017-04-03 19:15:05 +0300

    Add a request to json with jquery

--- app/get_tables_data.py
+++
@@ -1,5 +0,0 @@
-import json
-
-def read_the_file(file_name):
-    with open(file_name, "r", encoding="utf-8") as file:
-        return json.load(file)
--- app/views.py
+++ app/views.py
@@ -1,11 +1,8 @@
 from flask import render_template
 from app import app
-from app.get_tables_data import read_the_file
 
 
 @app.route('/')
 def index():
-    ege_data = read_the_file('app/static/Tables_data.json')
-    return render_template("index.html",
-    						ege_data=ege_data)
+    return render_template("index.html")
 
